4061b8a919f59d00d86aa89bc26929b0e59543e5
shiri92 <shiriron92@gmail.com>
2019-05-27 15:03:08 +0300

    Updates

---
+++ precache-manifest.2a074edff033c50b6294ab2e7332f26c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88142898ae975c52d77f0bd61e293b8d",
+    "url": "/questions-quiz/index.html"
+  },
+  {
+    "revision": "d802686efd01a0de6971",
+    "url": "/questions-quiz/static/css/2.dd6dd7bf.chunk.css"
+  },
+  {
+    "revision": "da3457ddc677589c4e49",
+    "url": "/questions-quiz/static/css/main.053b32d2.chunk.css"
+  },
+  {
+    "revision": "d802686efd01a0de6971",
+    "url": "/questions-quiz/static/js/2.413bd870.chunk.js"
+  },
+  {
+    "revision": "da3457ddc677589c4e49",
+    "url": "/questions-quiz/static/js/main.3e800d7a.chunk.js"
+  },
+  {
+    "revision": "d155a43ac8a4a80cd6bd",
+    "url": "/questions-quiz/static/js/runtime~main.28b35228.js"
+  },
+  {
+    "revision": "b39afe4a04caa099a5b91141d4362deb",
+    "url": "/questions-quiz/static/media/ConcertOne-Regular.b39afe4a.ttf"
+  },
+  {
+    "revision": "e124da5845f0517667e6ec639e4ca8f7",
+    "url": "/questions-quiz/static/media/background.e124da58.jpg"
+  }
+]);
